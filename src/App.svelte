c98c9903fe7d7e587ba98e5f821f6c1c21b372e2
HaffiziHasni <86970349+HaffiziHasni@users.noreply.github.com>
2022-01-07 19:22:13 +0800

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	let count=0;
+	function increment(){
+		count+=1;
+
+
+	}
+	function decrement(){
+		count-=1
+
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+</main>
+
+
+
+<div>Count:{count}</div>
+{#if count<5}
+<button on:click={increment}>Increment</button>
+{:else}
+<p>cannot increment</p>
+{/if}
+<button on:click={decrement}>Decrement</button>
